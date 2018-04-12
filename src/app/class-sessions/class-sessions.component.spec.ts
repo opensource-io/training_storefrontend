e508d81eaec3938414e865327eccecbe2e5f3685
Rob Jenks <rob@opensource.io>
2018-04-11 21:59:39 -0500

    fixing version problem

--- src/app/class-sessions/class-sessions.component.spec.ts
+++ src/app/class-sessions/class-sessions.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ChartsModule as Ng2Charts } from 'ng2-charts';
 
 import { PageHeaderModule } from '../page-header/page-header.module';
 
@@ -14,7 +13,6 @@ describe('ClassesComponent', () => {
     TestBed.configureTestingModule({
     imports: [
       RouterTestingModule,
-      Ng2Charts,
       PageHeaderModule
     ],
       declarations: [ ClassSessionsComponent ]
